325877111556f53a2849920381f84e07580115ed
haxbeysoftware-svg <haxbeysoftware@gmail.com>
2026-07-24 23:54:41 +0300

    Create VirtualCore.java

---
+++ src/main/java/com/example/sandboxcore/VirtualCore.java
@@ -0,0 +1,58 @@
+package com.example.sandboxcore.core;
+
+import android.app.ActivityThread;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.util.Log;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import com.example.sandboxcore.hook.VAInstrumentation;
+
+public class VirtualCore {
+
+    private static final String TAG = "VirtualCore";
+    private static final VirtualCore sInstance = new VirtualCore();
+
+    private Context mContext;
+    private Instrumentation mOriginalInstrumentation;
+    private VAInstrumentation mHookInstrumentation;
+
+    public static VirtualCore get() {
+        return sInstance;
+    }
+
+    public void startup(Context context) {
+        mContext = context.getApplicationContext();
+        try {
+            hookInstrumentation();
+            Log.i(TAG, "VirtualCore initialized successfully");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to hook instrumentation", e);
+        }
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
+
+    private void hookInstrumentation() throws Exception {
+        Object activityThread = getActivityThread();
+
+        Field instrumentationField = ActivityThread.class.getDeclaredField("mInstrumentation");
+        instrumentationField.setAccessible(true);
+
+        mOriginalInstrumentation = (Instrumentation) instrumentationField.get(activityThread);
+        mHookInstrumentation = new VAInstrumentation(mOriginalInstrumentation);
+
+        instrumentationField.set(activityThread, mHookInstrumentation);
+    }
+
+    private Object getActivityThread() throws Exception {
+        Method currentActivityThreadMethod =
+                ActivityThread.class.getDeclaredMethod("currentActivityThread");
+        currentActivityThreadMethod.setAccessible(true);
+        return currentActivityThreadMethod.invoke(null);
+    }
+}
